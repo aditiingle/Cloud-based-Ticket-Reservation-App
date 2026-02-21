d1d76cdc582116c7def007a3c1d48465476531dc
aditiingle <aditiingle14@gmail.com>
2026-02-21 11:29:13 -0500

    Connect Spring Boot app to MongoDB Atlas

--- src/main/java/com/ticket/reservation/ReservationApplication.java
+++ src/main/java/com/ticket/reservation/ReservationApplication.java
@@ -1,13 +1,31 @@
 package com.ticket.reservation;
 
+import com.ticket.reservation.model.User;
+import com.ticket.reservation.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class ReservationApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ReservationApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+//        User u = new User("Mike", "mike@email.com");
+//        userRepository.save(u);
+//
+//        userRepository.findAll().forEach(user ->
+//                System.out.println(user.getName() + " - " + user.getEmail())
+//        );
+    }
 
 }
---
+++ src/main/java/com/ticket/reservation/model/User.java
@@ -0,0 +1,27 @@
+package com.ticket.reservation.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "User") // Name of the collection inside reservation DB
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+}
